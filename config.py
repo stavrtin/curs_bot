6781a0d239a1a746f66db2e51b221b69e68bb51f
stavrtin <stavrtin@yandex.ru>
2024-08-15 18:21:58 +0300

    Работающий бот с автопостингом в канал

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # @cny_curs_bot
 #  1599819711
-# token = '6703618881:AAF4sczGTuQwAx0436qkc2AyQcorO9amM3g'
+token = '6703618881:AAF4sczGTuQwAx0436qkc2AyQcorO9amM3g'
 
 # @cny_curs2_bot
-token = '6907619291:AAHJGz0FMw_lFmo7JLjXlxbGJ0AuHlyRpSE'
+# token = '6907619291:AAHJGz0FMw_lFmo7JLjXlxbGJ0AuHlyRpSE'
 
 time1 = 10
 time2 = 12
